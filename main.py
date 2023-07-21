2cf3c160d4a3c4fe6ef0e0b62caa7e1ef5400072
Vanessamachado93 <machado.vanessa32@gmail.com>
2023-07-21 13:58:46 -0300

    Ajustes de melhores praticas, criando link de voltar para homepage suando o url_for

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def homepage():
     return render_template("homepage.html")
 
-@app.route("/perfil")
-def perfil():
-    return "Perfil do Usúario"
+@app.route("/perfil/<usuario>")
+def perfil(usuario):
+    return render_template("perfil.html", usuario=usuario)
 
 
 if __name__ == "__main__":
