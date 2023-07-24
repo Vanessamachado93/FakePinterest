8991170f448a812da0806210941cbb9b5cc2d427
Vanessamachado93 <machado.vanessa32@gmail.com>
2023-07-24 10:28:07 -0300

    Instalando as bibliotecas do Fask de login e bcrypt e adicionando as validações do form de login na homepage

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SECRET_KEY"] = "cf437d500dd7527720476c9f30dc5767"
 
 database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "homepage"
 
 from fakepinterest import routes
