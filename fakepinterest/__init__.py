18eb957b5c390a3a97edbba152adbb041da5f46b
Vanessamachado93 <machado.vanessa32@gmail.com>
2023-07-25 09:26:36 -0300

    Ajustes para na pasta static para adicionar a foto pradão quando o usuario fizer o cadastro de login

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -6,6 +6,7 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
 app.config["SECRET_KEY"] = "cf437d500dd7527720476c9f30dc5767"
+app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
