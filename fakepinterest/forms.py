09c00db7805ff8d5bccf91e9d07f50300df35527
Vanessamachado93 <machado.vanessa32@gmail.com>
2023-07-28 15:26:32 -0300

    Ajustes no login

--- fakepinterest/forms.py
+++ fakepinterest/forms.py
@@ -1,18 +1,23 @@
-# Criar o formularios
-
-from flask_wtf import  FlaskForm
+# criar os formularios do nosso site
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, FileField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
 from fakepinterest.models import Usuario
+
 
 class FormLogin(FlaskForm):
     email = StringField("E-mail", validators=[DataRequired(), Email()])
     senha = PasswordField("Senha", validators=[DataRequired()])
     botao_confirmacao = SubmitField("Fazer Login")
 
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if not usuario:
+            raise ValidationError("Usuário inexistente, crie uma conta")
+
 class FormCriarConta(FlaskForm):
     email = StringField("E-mail", validators=[DataRequired(), Email()])
-    username = StringField("Nome de Usúario", validators=[DataRequired()])
+    username = StringField("Nome de usuário", validators=[DataRequired()])
     senha = PasswordField("Senha", validators=[DataRequired(), Length(6, 20)])
     confirmacao_senha = PasswordField("Confirmação de Senha", validators=[DataRequired(), EqualTo("senha")])
     botao_confirmacao = SubmitField("Criar Conta")
@@ -20,7 +25,7 @@ class FormCriarConta(FlaskForm):
     def validate_email(self, email):
         usuario = Usuario.query.filter_by(email=email.data).first()
         if usuario:
-            return ValidationError("E-mail já cadastrado, faça login para continuar")
+            raise ValidationError("E-mail já cadastrado, faça login para continuar")
 
 
 class FormFoto(FlaskForm):
